4a57ef556a2a81571fa709af89e70465b6a4ec2c
Rodrigo Soria <rodsilver83@hotmail.com>
2013-04-01 12:48:21 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FisrtApp::Application.config.secret_token = '9e287a3f8175c4fc63e6303876e8ec36194f57bc04c75acb9e97ba0e1d59e697f0e2a2a2a97f5cc53ff515dc3839b2c87f587a53263591f16d6244fd596887ec'
